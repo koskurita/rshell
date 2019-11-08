53783cb58961610b82f64590a4d45cc74ac04545
Robert Wyatt <rwyat002@ucr.edu>
2019-11-08 10:27:15 -0800

    many changes to executableCommands and line function

--- prototype/parserprototype.cpp
+++ prototype/parserprototype.cpp
@@ -26,89 +26,89 @@ class Line: public UserInput {
     protected:
     private:
 /*        int ID = 2;*/
-
+	vector <UserInput*> Inputs;
     public:
-        int ParseUserInput();
-        vector<UserInput*> Inputs;
-        void doInput()
-	
-	UserInput * ptr = Inputs[0];
-	int currInput = 0;
-	int iterInt = 0;
-	char * tempCharContainer[50];
-	while(ptr->ID < 100){
-	
-	if(currInput < Inputs.size())
-	{currInput++;}
-	else   //This is just incase we reach the end of our vector
-	{return;}
-	ptr = Inputs[currInput];
-	}
-	
-	//here we have to get the const char* from executable command and put it into our tempcharContainer
-	while(iterInt < 50){
-	if(/*some condition that shows that we are at end of command array is != true*/)
-	tempCharContainer[iterInt] = strdup(command[iterInt]);
-	else{
-	tempCharContainer[iterInt] = NULL;
-	iterInt = 50;
-	}
-	iterInt++;
-	
-	}
-	
-	
-	
-	pid_t child;
-	child = fork();
-	if (child < 0){
-	std::cout << "Massive error, fork failed." << endl;
-	}
-	//else if (InputVector[i]->PassOrFail == 0){ //This is all be to check if This executable has already "failed" due to a '&&' or '||'
-	////   //does nothing if passorfial ==0
-	//}
-	else if (child == 0) {
-	if(execvp(tempCharContainer[0], tempCharContainer) < 0){  //this will be (*InputVector[i-1]->words, InputVector[i-1]->words)
-	cout << "failed to exe,delete this mssg later."; //delete this msg later
-	PassOrFail = 0;      //signals that user input failed. Will be InputVector[i-1]->PassOrFail = 0;
-	}
-	}
-	else{
-	PassOrFail = 1;      //Will be InputVector[i-1]->PassOrFail = 1;
-	cout<< PassOrFail;
-	waitpid(-1,&child,0);
-	
-	}
-	
-	iterInt = 0;
-	while(iterInt < 50){
-        if(tempCharContainer[iterInt] != NULL)
-        free(tempCharContainer[iterInt]);
-        else{
-        iterInt = 50;
-        }
-        iterInt++;
+	Line() = default;
 
-        }
-	return 1;
+	~Line(){//This destructor should call the destructor of every element in the vector and then pop it. 
+	int lastElement = Input.size() - 1;
+	while (lastElement != -1){
+	Inputs->~Inputs[lastElement];
+	Inputs.pop_back();
+	lastElement--;
+	}
 	
+	
+	}
+
+        int ParseUserInput();
+
+        void doInput(){
+	int IterInt = 0;
+	while(IterInt < Inputs.size())
+	if (Inputs[IterInt]->ID < 100)
+	{
+	Inputs[IterInt]->doInput();//call do input on executable/line
+	IterInt++;
+	Inputs[InterInt]->PerformNext(Inputs[IterInt - 1],Inputs[IterInt + 1]);//call perform next on the symbol
+	}
+	else
+	{
+	IterInt++;//do nothing and skip one because this is a symbol
+	}
+}
 };
 
 class ExecutableCommand: public UserInput { // USE CONST CHAR
     private:
-        const char** command;  //remove const
-/*        int passOrFail = -1;
-        int ID = 1;*/
+        char* command[50];  //remove const
+        int passOrFail = -1;
+        int ID = 1;
     public:
 /*        void SetPassOrFail(int oneOrZero); // this function sets pass or fail to one or zero
         int ReturnPassOrFail(){return this->passOrFail}; //Returns pass or fail integer*/
+
         ExecutableCommand(const char* words[50]){ // constructor
-            command = words;
-		//here we convert from const char* to char*
+        int iterInt = 0;
+	while(words[iterInt] != NULL){
+	command[iterInt] = strdup(words[iterInt]);
+	iterInt++;
+	}    
+	command[IterInt + 1] = NULL;
+	
         }
+
+
 	~ExecutableCommand(){
-		//free all elements of 'command' array here
+	int iterInt = 0;
+	while(words[iterInt] != NULL){
+        free(words[iterInt]);
+        iterInt++;
+        }
 	}
+
+	void doInput(){//this is now neccessary
+	pid_t child;
+        child = fork();
+        if (child < 0){
+        std::cout << "Massive error, fork failed." << endl;
+        }
+        else if (this->PassOrFail == 0){ //This is all be to check if This executable has already "failed" due to a '&&' or '||'
+       
+        }
+         else if (child == 0) {
+        if(execvp(command[0], command) < 0){  //this will be (*InputVector[i-1]->words, InputVector[i-1]->words)
+        cout << "failed to exe,delete this mssg later."; //delete this msg later
+        this->PassOrFail = 0;      //signals that user input failed. Will be InputVector[i-1]->PassOrFail = 0;
+        }
+        }
+        else{
+        this->PassOrFail = 1;      //Will be InputVector[i-1]->PassOrFail = 1;
+        waitpid(-1,&child,0);
+        }
+	
+	}
+	
 	
 };
 
@@ -173,10 +173,8 @@ else
 
 
 void ParseUserInput(string cheese){
-    
     string s = "";
     vector<string> temp_vector;
-    
     for(unsigned int i = 0; i < cheese.size(); i++){
         if(cheese[i] == ' '){
             s += cheese[i];
@@ -211,11 +209,11 @@ void ParseUserInput(string cheese){
     }
     temp_vector.push_back(s);
     
-    
+
     for(unsigned int i = 0; i < temp_vector.size(); i++){
         std::cout << temp_vector[i] << std::endl;
     }
-    
+
 
     vector<const char*> ggs;
     const char* charArray;
@@ -223,20 +221,21 @@ void ParseUserInput(string cheese){
         charArray = temp_vector[i].c_str();
         ggs.push_back(charArray);
     }
-    
+
     for(unsigned int i = 0; i < ggs.size(); i++){
         cout << ggs[i] << endl;
     }
-    
+
     const char* doubleAnd = "&&";
     const char* doubleOr = "||";
     const char* semicolon = ";";
     Line *new_line = new Line;
     int k;
     const char* temp[50];
-    
     for(unsigned int i = 0; i < ggs.size(); i++){
-	    temp.fill(NULL);
+	        for(int y =0; y < 50;y++){
+                temp[y] = NULL;
+            }
 	    k = 0;
             while(ggs[i] != doubleAnd && ggs[i] != doubleOr && ggs[i] != semicolon && i < ggs.size()){
                 temp[k] = ggs[i];
@@ -246,7 +245,7 @@ void ParseUserInput(string cheese){
             if(i == ggs.size()){
                 UserInput* new_executable_command = new ExecutableCommand(temp);
                 new_line->Inputs.push_back(new_executable_command);
-                return;
+                break;
             }
             else{
                 UserInput* new_executable_command = new ExecutableCommand(temp);
@@ -255,7 +254,6 @@ void ParseUserInput(string cheese){
                 new_line->Inputs.push_back(new_symbol);
             }
     }
-    
     return;
 }
 
