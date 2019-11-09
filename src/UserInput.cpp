c052a2eee149797edb66af1c3e14d897cd891df7
koskurita <55513260+koskurita@users.noreply.github.com>
2019-11-08 16:33:44 -0800

    Uploaded UserInput

---
+++ src/UserInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "UserInput.hpp"
+
+    int UserInput::ReadID(){
+        return ID;
+    }
+        
+    int UserInput::returnID(){
+        return ID;
+    }
+        
+    int UserInput::returnPassOrFail(){
+        return passOrFail;
+    }
