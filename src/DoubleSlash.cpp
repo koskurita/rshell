e8091ac5b68d2f0e290fbc3724eb4bd9fbf2ade1
koskurita <55513260+koskurita@users.noreply.github.com>
2019-11-08 16:31:28 -0800

    Uploaded DoubleSlash

---
+++ src/DoubleSlash.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "DoubleSlash.hpp"
+
+    DoubleSlash::DoubleSlash(const char * s):Symbol(s){//default constructor
+        ID = 102;
+    }
+
+    bool DoubleSlash::PerformNext(UserInput * one, UserInput * two){
+        if (one->returnPassOrFail() == 1){
+            two->SetPassOrFail(0);
+            return false;
+        }
+        else{
+            //do nothing
+            return true;
+        }
+    }
