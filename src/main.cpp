b316ba177de27d605004bab32b4b67bd45f29efd
Robert Wyatt <rwyat002@ucr.edu>
2019-11-08 16:05:07 -0800

    made changes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,21 @@
+//#include <> everything
+//
+#include <string>
+using namespace std;
 
+int main(){
+UserInput * mainVec = new Line();
+
+string input = "";
+while (input != "exit" || input != "Exit"){
+cout << "$";
+getline(cin,input);
+cout << endl;
+mainVec->parseUserInput(input);
+}
+
+
+
+
+
+}
