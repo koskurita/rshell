cadce6ab16a6c144fe3ad4df775e77b7ce9b6268
koskurita <55513260+koskurita@users.noreply.github.com>
2019-11-08 16:29:08 -0800

    Uploaded Line

---
+++ header/Line.hpp
@@ -0,0 +1,32 @@
+#ifndef __LINE_H__
+#define __LINE_H__
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <queue>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <stdlib.h>
+#include  <sys/types.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+class Line: public UserInput {
+    private:
+        int ID = 2;
+    vector<UserInput*> Inputs;
+    int passOrFail = -1;
+    public:
+    Line();
+    ~Line();
+    void SetPassOrFail(int oneOrZero){;
+    int ReturnPassOrFail();
+    bool PerformNext(UserInput *one, UserInput*two);
+    void ParseUserInput(std::string cheese);
+    void doInput();
+};
+
+#endif /*__LINE_H__*/
