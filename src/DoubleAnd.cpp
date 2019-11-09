921d9d20da2ce841bf6875ff78ec413c5482bf9b
koskurita <55513260+koskurita@users.noreply.github.com>
2019-11-08 16:31:04 -0800

    Uploaded DoubleAnd

---
+++ src/DoubleAnd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "DoubleAnd.hpp"
+
+    DoubleAnd::DoubleAnd(const char * s):Symbol(s){//default constructor
+        ID = 103;
+    }
+
+    bool DoubleAnd::PerformNext(UserInput * one, UserInput * two){
+        if (one->returnPassOrFail() == 1){
+            //do nothing
+            return true;
+        }
+        else{
+            two->SetPassOrFail(0);
+            return false;
+        }
+    }
+
+
+
