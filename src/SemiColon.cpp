00c34d20cbb3402f92887494b1069fa96cd49c45
koskurita <55513260+koskurita@users.noreply.github.com>
2019-11-08 16:32:47 -0800

    Uploaded SemiColon

---
+++ src/SemiColon.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Semicolon.hpp"
+
+
+    bool SemiColon::PerformNext(UserInput * one, UserInput * two){
+        if (one->returnPassOrFail() == 1){
+            //do nothing
+            return true;
+        }
+        else
+            //do nothing
+            return true;
+        }
+        
+        
+    SemiColon::SemiColon(const char* s):Symbol(s){//default constructor
+        ID = 101;
+    }
