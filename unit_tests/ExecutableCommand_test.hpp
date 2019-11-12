9133b61eab630ff743fa7a49b82fd2e2f1fe431d
koskurita <55513260+koskurita@users.noreply.github.com>
2019-11-12 10:07:03 -0800

    Update ExecutableCommand_test.hpp

---
+++ unit_tests/ExecutableCommand_test.hpp
@@ -0,0 +1,19 @@
+#include "UserInput.cpp"
+#include "Line.cpp"
+#include "ExecutableCommand.cpp"
+#include "gtest/gtest.h"
+
+TEST(UserInputTest, TestSetPassOrFail) {
+    UserInput * tester = new ExecutableCommand();
+    tester->SetPassOrFail(1);
+    EXPECT_EQ(tester->returnPassOrFail(), 1);
+
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+EXPECT_EQ(tester->returnPassOrFail(), 1);
+    
+}
