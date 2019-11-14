9c01b2e6955323e746be573a453abede645ee960
koskurita <55513260+koskurita@users.noreply.github.com>
2019-11-13 22:13:24 -0800

    Update DoubleAnd_test.hpp

--- unit_tests/DoubleAnd_test.hpp
+++ unit_tests/DoubleAnd_test.hpp
@@ -1,1 +1,16 @@
 
+#include "everything.hpp"
+
+
+TEST(UserInputTest, TestSetPassOrFail) {
+    UserInput * tester = new DoubleAnd();
+    tester->SetPassOrFail(1);
+    EXPECT_EQ(tester->returnPassOrFail(), 1);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+EXPECT_EQ(tester->returnPassOrFail(), 1);
+    
