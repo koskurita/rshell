316a2bb2f3f158d51124c0833a2a7727617bb08c
koskurita <55513260+koskurita@users.noreply.github.com>
2019-11-13 23:05:29 -0800

    Update SemiColon.cpp

--- unit_tests/SemiColon.cpp
+++ unit_tests/SemiColon.cpp
@@ -7,5 +7,8 @@ TEST(SemiColonTest, TestSetPassOrFail) {
     tester->SetPassOrFail(1);
     EXPECT_EQ(tester->returnPassOrFail(), 0);
 }
-
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
 
