5b80a818eefc6a7370337ad04008e35597b68374
Robert Wyatt <rwyat002@ucr.edu>
2019-11-14 19:54:11 -0800

    deleted unnessary integration tests

--- integration_tests/single_command_tests.sh
+++ integration_tests/single_command_tests.sh
@@ -3,10 +3,10 @@
 INPUTS=("ls" "echo hello" "mkdir test" "rm -r test")
 
 echo "Executing ./c-echo ${INPUTS[3]}"
-./main ${INPUTS[3]}
+./main.cpp ${INPUTS[3]}
 echo "Executing ./c-echo ${INPUTS[2]}"
-./main ${INPUTS[2]}
+./main.cpp ${INPUTS[2]}
 echo "Executing ./c-echo ${INPUTS[1]}"
-./main ${INPUTS[1]}
+./main.cpp ${INPUTS[1]}
 echo "Executing ./c-echo ${INPUTS[0]}"
-./main ${INPUTS[0]}
+./main.cpp ${INPUTS[0]}
